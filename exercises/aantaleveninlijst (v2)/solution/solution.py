cd3af9f3af33c0e3d247a82d507b05e0f2b02199
DannyMeeusen-KLA <danny.meeusen@kla.be>
2024-11-23 09:45:14 +0100

    aantal even, extra commentaar

--- exercises/aantaleveninlijst (v2)/solution/solution.py
+++ exercises/aantaleveninlijst (v2)/solution/solution.py
@@ -4,12 +4,19 @@ zin = input('Geef de getallen in, gescheiden door spaties:')
 # omvormen tot stringlijst
 strlijst = zin.split(' ')
 
+# aantal elementen in de lijst ophalen (wordt gebruikt in de range)
+aantalinlijst = len(strlijst)
+
 # teller aantal even getallen op zijn beginwaarde zetten
 aantaleven = 0
 
-for strgetal in strlijst:
-    intgetal = int(strgetal)
+# door de lijst lopen (van index 0 tot aantalinlijst-1)
+for tel in range(0, aantalinlijst, 1):
+    # elk element uit de lijst (van type string) omzetten naar type int
+    intgetal = int(strlijst[tel])
+    # als het getal even is: teller aantal even getallen met één verhogen
     if intgetal % 2 == 0:
         aantaleven = aantaleven + 1
 
+# aantal even getallen afdrukken
 print(aantaleven)
