931bd2e7b9673012d5c7e725d5a62f8c3e03c06c
DannyMeeusen-KLA <danny.meeusen@kla.be>
2024-11-23 09:58:50 +0100

    verdubbelen of halveren, extra commentaar

--- exercises/verdubbelofhalveren (v2)/solution/solution.py
+++ exercises/verdubbelofhalveren (v2)/solution/solution.py
@@ -4,14 +4,21 @@ zin = input('Geef de getallen in, gescheiden door spaties:')
 # omvormen tot stringlijst
 strlijst = zin.split(' ')
 
+# bepalen aantal elementen in de lijst
+aantalinlijst = len (strlijst)
+
 # lege lijst aanmaken
 lijst = []
 
-for strgetal in strlijst:
-    intgetal = int(strgetal)
+# door de lijst lopen voor index = 0 tot en met index = aantalinlijst - 1
+for tel in range(0, aantalinlijst, 1):
+    # het element in de lijst (type = string) omzetten naar type int
+    intgetal = int(strlijst[tel])
+    # nakijken of het bekomen getal even of oneven is, de correcte bewerking erop uitvoeren en toevoegen aan de uitvoerlijst
     if intgetal % 2 == 0:
         lijst.append (intgetal // 2)
     else:
         lijst.append (intgetal * 2)
 
+# de uitvoerlijst afdrukken
 print(lijst)
