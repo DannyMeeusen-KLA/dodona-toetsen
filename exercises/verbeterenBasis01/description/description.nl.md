c0d46c73c9275ac3d2155f5847c8569b38ce131e
DannyMeeusen-KLA <danny.meeusen@kla.be>
2025-08-23 14:23:52 +0200

    VerbeterenBasis01

---
+++ exercises/verbeterenBasis01/description/description.nl.md
@@ -0,0 +1,42 @@
+Je krijgt een oplossing van onderstaande opgave, jammer genoeg met syntax- en/of logische fouten.  
+Test het programma in de sandbox, corrigeer de fouten en dien je gecorrigeerde oplossing in.  
+  
+De gebruiker geeft drie getallen in:  
+- twee gehele getallen (een *int*), en  
+- één decimaal (een *float*).  
+  
+Het programma drukt achtereenvolgens het volgende af:  
+- het eerst ingebrachte getal  
+- de som van de eerste twee getallen  
+- het derde getal  
+- het product van de som met het derde getal, afgerond tot op 1 decimaal (gebruik de functie *round*).      
+  
+
+### Voorbeelden
+
+```console?lang=python&prompt=>>>
+>>> 2
+>>> 4
+>>> 3.6
+2
+6
+3.5
+21.0
+
+>>> 1
+>>> 6
+>>> 2.11
+1
+7
+2.11
+14.8
+
+>>> 1
+>>> 1
+>>> 1.22
+1
+2
+1.22
+2.4
+
+```
---
+++ exercises/verbeterenBasis01/solution/solution.py
@@ -0,0 +1,21 @@
+# In dit programma zitten veel fouten. 
+# Wij tellen al zeker 8 nodige aanpassingen die er moeten gebeuren vooraleer het perfect werkt.
+
+getal1 = int(input(Geef een geheel getal in:))              # int toegevoegd
+getal2 = int(input(Geef een geheel getal in:))              # int toegevoegd
+getal3 = float(input(Geef een decimaal getal in:))          # float toegevoegd
+
+# Druk het eerste getal af
+print (getal1)                                              # print aan kantlijn gezet en getal1 met kleine letter
+
+# Bereken de som van getal1 en getal2 en druk af
+som = getal1 + getal2                                       # toekenning is van rechts naar links
+print (som)
+
+# Druk het derde getal af
+print (getal3)                                              # aanhalingstekens verwijderen
+
+# Bereken het product van de som en getal3, rond af tot op 1 cijfer na de komma en druk af
+product = som * getal3
+product = round(product, 1)                                 # 'product =' toevoegen aan linkerkant (toekenning)
+print (product)
