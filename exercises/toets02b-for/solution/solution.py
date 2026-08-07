8babd7938406c37aefd59dbea619f5928fd8f737
DannyMeeusen-KLA <danny.meeusen@kla.be>
2026-08-07 14:40:24 +0200

    v03

--- exercises/toets02b-for/solution/solution.py
+++ exercises/toets02b-for/solution/solution.py
@@ -8,4 +8,4 @@ volgnummer = int(input('Geef het volgnummer van de gewenste kleur: '))
 if volgnummer < 1 or volgnummer > 7:
     print ('Met dit volgnummer komt geen kleur overeen.')
 else:
-    print ('Met volgnummer', volgnummer, 'komt de kleur ', regenboog[volgnummer-1],'overeen.')
+    print ('Met volgnummer', volgnummer, 'komt de kleur', regenboog[volgnummer-1],'overeen.')
